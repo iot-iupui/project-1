60b76d2c208736a9a9d506c6edc4f4b348875fdf
Nicholas Hubbard <nrhubbar@iupui.edu>
2017-09-28 21:05:06 -0400

    Wrote code to flicker and shorten flicker time every few seconds

--- ControlGpioExample.java
+++ ControlGpioExample.java
@@ -53,37 +53,51 @@ public class ControlGpioExample {
 
         // set shutdown state for this pin
         pin.setShutdownOptions(true, PinState.LOW);
+        runLightFrequency(pin);
+        // System.out.println("--> GPIO state should be: ON");
 
-        System.out.println("--> GPIO state should be: ON");
+        // Thread.sleep(5000);
 
-        Thread.sleep(5000);
+        // // turn off gpio pin #01
+        // pin.low();
+        // System.out.println("--> GPIO state should be: OFF");
 
-        // turn off gpio pin #01
-        pin.low();
-        System.out.println("--> GPIO state should be: OFF");
+        // Thread.sleep(5000);
 
-        Thread.sleep(5000);
+        // // toggle the current state of gpio pin #01 (should turn on)
+        // pin.toggle();
+        // System.out.println("--> GPIO state should be: ON");
 
-        // toggle the current state of gpio pin #01 (should turn on)
-        pin.toggle();
-        System.out.println("--> GPIO state should be: ON");
+        // Thread.sleep(5000);
 
-        Thread.sleep(5000);
+        // // toggle the current state of gpio pin #01  (should turn off)
+        // pin.toggle();
+        // System.out.println("--> GPIO state should be: OFF");
 
-        // toggle the current state of gpio pin #01  (should turn off)
-        pin.toggle();
-        System.out.println("--> GPIO state should be: OFF");
+        // Thread.sleep(5000);
 
-        Thread.sleep(5000);
-
-        // turn on gpio pin #01 for 1 second and then off
-        System.out.println("--> GPIO state should be: ON for only 1 second");
-        pin.pulse(1000, true); // set second argument to 'true' use a blocking call
+        // // turn on gpio pin #01 for 1 second and then off
+        // System.out.println("--> GPIO state should be: ON for only 1 second");
+        // pin.pulse(1000, true); // set second argument to 'true' use a blocking call
 
         // stop all GPIO activity/threads by shutting down the GPIO controller
         // (this method will forcefully shutdown all GPIO monitoring threads and scheduled tasks)
         gpio.shutdown();
 
         System.out.println("Exiting ControlGpioExample");
+
+    }
+
+    public static void runLightFrequency(GpioPinDigitalOutput pin) {
+         pin.low();
+         
+         while(true){
+             for(int startingInterval = 1000; startingInterval > 0; startingInterval -= 150){
+                 for(int i = 0; i * startingInterval < 3000; i++){
+                    pin.toggle();
+                    Thread.sleep(startingInterval);
+                 }
+             }
+         }
     }
 }
